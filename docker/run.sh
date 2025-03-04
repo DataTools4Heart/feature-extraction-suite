d4efc7f6b183eb3904f10a941c8efa722d539741
Dogukan Cavdaroglu <dogukan@srdc.com.tr>
2025-03-04 15:01:51 +0300

    :sparkles: Modify docker setup to run onfhir-feast-server

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker compose -f feature-extraction-suite/docker/docker-compose.yml --project-directory ./ run onfhir-feast-cli extract-p -jobId feature-extraction-job-test -name study1
+#docker compose -f feature-extraction-suite/docker/docker-compose.yml --project-directory ./ run onfhir-feast-cli extract-p -jobId feature-extraction-job-test -name study1
 
 # Interactive feast
-docker compose -f feature-extraction-suite/docker/docker-compose.yml --project-directory ./ run -it onfhir-feast-cli -it
+#docker compose -f feature-extraction-suite/docker/docker-compose.yml --project-directory ./ run -it onfhir-feast-cli -it
+
+docker compose -f feature-extraction-suite/docker/docker-compose.yml --project-directory ./ -p dt4h-onfhir-feast up -d
