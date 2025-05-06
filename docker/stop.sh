060660136c4f94f97474d7254b9403d8a4ca9a23
yemregurses <gursesyunus@hotmail.com>
2025-05-06 13:21:13 +0300

    :rocket: remove volume deletion from Docker stop script

--- docker/stop.sh
+++ docker/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f feature-extraction-suite/docker/docker-compose.yml -p dt4h-onfhir-feast down -v
+docker compose -f feature-extraction-suite/docker/docker-compose.yml -p dt4h-onfhir-feast down
