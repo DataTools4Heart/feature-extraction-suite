c52c27ee3735b33ffed7f4f27cfe0061e4c3eb37
yemregurses <gursesyunus@hotmail.com>
2025-07-23 16:22:58 +0300

    :rocket: update onfhir-feast image to use 'dt4h' tag in docker-compose and pull script

--- docker/pull.sh
+++ docker/pull.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Pulling onfhir-feast image from private SRDC repo..."
 docker login nexus.srdc.com.tr:18445 --username tofhir --password-stdin < srdc-docker-registry-password.txt
-docker pull nexus.srdc.com.tr:18445/srdc/onfhir-feast:latest
-docker tag nexus.srdc.com.tr:18445/srdc/onfhir-feast:latest srdc/onfhir-feast:latest
+docker pull nexus.srdc.com.tr:18445/srdc/onfhir-feast:dt4h
+docker tag nexus.srdc.com.tr:18445/srdc/onfhir-feast:dt4h srdc/onfhir-feast:dt4h
 
 
 
