7770063590432af102b92285f563c5cefda9c4f9
yemregurses <gursesyunus@hotmail.com>
2025-08-13 10:44:43 +0300

    :rocket: add cleanup for output-data/ directory

--- docker/clean-and-stop.sh
+++ docker/clean-and-stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# remove output-data directory
+rm -rf feature-extraction-suite/output-data/
 docker compose -f feature-extraction-suite/docker/docker-compose.yml -p dt4h-onfhir-feast down -v
