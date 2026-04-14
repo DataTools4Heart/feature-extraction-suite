7c65115e58bf32ef4aad7218df3e743275f7bb6b
Dogukan Cavdaroglu <dogukan@srdc.com.tr>
2026-04-14 11:45:10 +0300

    :wrench: build/conf: Replace nexus.srdc.com.tr:18445 by docker.srdc.com.tr

--- docker/pull.sh
+++ docker/pull.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Pulling onfhir-feast image from private SRDC repo..."
-docker login nexus.srdc.com.tr:18445 --username tofhir --password-stdin < srdc-docker-registry-password.txt
-docker pull nexus.srdc.com.tr:18445/srdc/onfhir-feast:latest
-docker tag nexus.srdc.com.tr:18445/srdc/onfhir-feast:latest srdc/onfhir-feast:latest
+docker login docker.srdc.com.tr --username tofhir --password-stdin < srdc-docker-registry-password.txt
+docker pull docker.srdc.com.tr/srdc/onfhir-feast:latest
+docker tag docker.srdc.com.tr/srdc/onfhir-feast:latest srdc/onfhir-feast:latest
 
 
 
